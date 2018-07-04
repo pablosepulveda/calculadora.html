a8669b145c8795fb85d6e8bfa53316606efcf823
pablosepulveda <pasadapa@hotmail.com>
2018-07-04 17:58:53 -0500

    cambio

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+var queCalcular = prompt ("que operacion deseas hacer?") 
+
+if (calcular == "volumen de un cilindro") {
+	var radio = prompt ("radio"); 
+	var altura = prompt ("altura");
+	var exponente = radio *= radio;
+	var pi = exponente *= 3.14159265359;
+	var final = pi *= altura;
+	alert ("el volumen de tu cilindro es " + final);
+}
+
+if (calcular == "transformar onzas a toneladas"); {
+	var onzas = prompt ("onzas");
+	var toneladas = onzas = 0.0000284395;
+alert (onzas + "onzas equivalen a" + toneladas);
+}
